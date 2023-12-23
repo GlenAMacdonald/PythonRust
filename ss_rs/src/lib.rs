31746d3ad1703c88952edb3e5235b01e5ba12ca3
Spares <macdonald.glen.a@gmail.com>
2023-12-24 01:07:09 +1100

    cleanup

--- ss_rs/src/lib.rs
+++
@@ -1,8 +0,0 @@
-/// Formats the sum of two numbers as string.
-pub fn sum_as_string(a: usize, b: usize) -> Result<String, String> {
-    let c = a + b;
-    if c > 10 {
-        return Err("Dummy Error, greater than 10".to_string())
-    }
-    Ok(c.to_string())
-}
